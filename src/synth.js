8b96da5c4c53d55d6f355a39d88b145c157746f5
Emil Ng <github@alphabeticaldisorder.com>
2015-02-24 17:53:17 -0500

    add audio analyser and visualizer

--- src/synth.js
+++ src/synth.js
@@ -230,11 +230,43 @@ var updateKeyboard = function(data) {
 };
 
 var getUpdateHandler = function(data) {
+  var visualizer = data.ui.visualizer;
+  var analyser = data.sound.analyser;
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.fftSize;
+  var dataArray = new Uint8Array(bufferLength);
+
+  var ctx = visualizer.getContext('2d');
+  var width = visualizer.width;
+  var height = visualizer.height;
+
   var update = function() {
     if (data.notesChanged) {
       updateKeyboard(data);
       data.notesChanged = false;
     }
+    analyser.getByteTimeDomainData(dataArray);
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = 'rgb(200, 200, 200)';
+    ctx.fillRect(0, 0, width, height);
+    ctx.beginPath();
+
+    var sliceWidth = width * (1.0/bufferLength);
+    var x  = 0;
+    for (var i = 0; i < bufferLength; i++) {
+      var v = dataArray[i] / 128.0;
+      var y = v * height/2;
+      if (i === 0) {
+        ctx.moveTo(x, y);
+      } else {
+        ctx.lineTo(x, y);
+      }
+      x += sliceWidth;
+    }
+
+    ctx.lineTo(width, height/2);
+    ctx.stroke();
+
     requestAnimationFrame(update);
   };
   return update;
@@ -249,13 +281,17 @@ var main = function() {
   var masterGain = context.createGain();
   masterGain.gain.value = 0.5;
   var compressor = context.createDynamicsCompressor();
+  var analyser = context.createAnalyser();
   masterGain.connect(compressor);
-  compressor.connect(context.destination);
+  compressor.connect(analyser);
+  analyser.connect(context.destination);
+  var visualizer = document.getElementById('visualizer');
   var data = {
     sound: {
       context:context,
       gainNodes: [],
       masterGain: masterGain,
+      analyser: analyser,
       octave: 3,
       notes: null,
       activeNotes: activeNotes
@@ -264,7 +300,8 @@ var main = function() {
       keyChars: keyChars,
       keyboard: null,
       keyDimensions: null,
-      keyOffsets: []
+      keyOffsets: [],
+      visualizer: visualizer
     },
     notesChanged: true
   };
