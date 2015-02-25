c4e300857f76b137ba892e1047d434fd72550df0
Emil Ng <github@alphabeticaldisorder.com>
2015-02-25 10:16:17 -0500

    use gulp to build project

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var es6ify = require('es6ify');
+var gulp = require('gulp');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
+var paths = ['./src/**/*.jsx', './src/**/*.js'];
+
+var getBundleName = function () {
+  // var version = require('./package.json').version;
+  var name = require('./package.json').name;
+  return name + '.' + 'min';
+};
+
+gulp.task('js', function() {
+  // place code for your default task here
+  var bundler = browserify({
+    entries: ['./src/synth.js'],
+    debug: true
+  });
+
+  var bundle = function() {
+    bundler.transform(reactify);
+    bundler.transform(es6ify.configure(/.jsx/));
+    bundler
+      .bundle()
+      .pipe(source(getBundleName() + '.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist/'));
+  };
+  return bundle();
+
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths, ['js']);
+});
+
+gulp.task('default', ['watch','js']);
