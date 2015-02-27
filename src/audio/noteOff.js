67b9235816f5dbbc543f68ba75c0643426635caf
Emil Ng <github@alphabeticaldisorder.com>
2015-02-27 12:32:14 -0500

    move variable declaration and location of notesChanged in noteOff function

--- src/audio/noteOff.js
+++ src/audio/noteOff.js
@@ -1,25 +1,25 @@
 var noteOff = function(data, keyIndex) {
   var audio = data.audio;
   var now = audio.context.currentTime;
+  var gainNode;
   var isNoteActive = ((keyIndex !== undefined) && (audio.activeNotes[keyIndex] === true));
   if (isNoteActive) {
-    var gainNode = audio.gainNodes[keyIndex];
+    gainNode = audio.gainNodes[keyIndex];
     gainNode.gain.cancelScheduledValues(now);
     gainNode.gain.setTargetAtTime(0, now, 0.1);
     audio.activeNotes[keyIndex] = false;
-    data.notesChanged = true;
   } else {
     audio.activeNotes = audio.activeNotes.map(function(isNoteActive, index) {
       if (isNoteActive) {
-        var gainNode = audio.gainNodes[index];
+        gainNode = audio.gainNodes[index];
         gainNode.gain.cancelScheduledValues(now);
         gainNode.gain.setTargetAtTime(0, now, 0.1);
         audio.activeNotes[keyIndex] = false;
       }
       return false;
     });
-    data.notesChanged = true;
   }
+  data.notesChanged = true;
 };
 
 module.exports = noteOff;
