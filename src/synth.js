8cfa30993d5284be86cbb8490ea7972b0aee04eb
Emil Ng <github@alphabeticaldisorder.com>
2015-03-06 20:41:56 -0500

    add envelopeUI

--- src/synth.js
+++ src/synth.js
@@ -12,6 +12,7 @@ var clickHandlers = require('./ui/clickHandlers.js');
 var keyHandlers = require('./ui/keyHandlers');
 
 // ui update
+var updateEnvelope = require('./ui/updateEnvelope.js');
 var updateKeyboard = require('./ui/updateKeyboard.js');
 var getDrawVisualizerHandler = require('./ui/drawVisualizer.js');
 
@@ -21,6 +22,10 @@ var getUpdateHandler = function(data) {
     if (data.notesChanged) {
       updateKeyboard(data);
       data.notesChanged = false;
+    }
+    if (data.envelopeChanged) {
+      updateEnvelope(data);
+      data.envelopeChanged = false;
     }
     drawVisualizer();
 
@@ -49,6 +54,18 @@ var main = function() {
       analyser: analyser,
       octave: 3,
       notes: null,
+      envelope: {
+        delayTime: 0,
+        attackTime: 0.1,
+        decayTime: 0.1,
+        sustainTime: 0,
+        releaseTime: 0.1,
+        delayValue: 0,
+        attackValue: 1,
+        decayValue: 0.5,
+        sustainValue: 0.5,
+        releaseValue: 0
+      },
       activeNotes: activeNotes
     },
     ui: {
@@ -56,8 +73,19 @@ var main = function() {
       keyboard: null,
       keyDimensions: null,
       keyOffsets: [],
+      envelope: {
+        width: 400,
+        height: 150,
+        minSpacing: 20,
+        maxSpacing: 60,
+        marginY: 20,
+        marginX: 25,
+        // enough room to give the 4 elements before it maxSpacing
+        releaseMin: 295,
+      }
     },
-    notesChanged: true
+    notesChanged: true,
+    envelopeChanged: true
   };
 
   data.audio.notes = generateNoteMappings();
---
+++ src/ui/updateEnvelope.js
@@ -0,0 +1,157 @@
+var updateEnvelope = function(data) {
+  var audioEnvelope = data.audio.envelope;
+  var envelopeData = data.ui.envelope;
+
+  var envelopeSvg = document.getElementById('svg-envelope');
+  var width = envelopeSvg.getAttributeNS(null, 'width');
+  var height = envelopeSvg.getAttributeNS(null, 'height');
+
+  var releaseMin = envelopeData.releaseMin;
+  var minSpacing = envelopeData.minSpacing;
+  var maxSpacing = envelopeData.maxSpacing;
+  var marginX = envelopeData.marginX;
+  var marginY = envelopeData.marginY;
+  var bottomY = height - marginY;
+  var drawHeight = height - (marginY * 2);
+
+  var propNames = ['delay', 'attack', 'decay', 'sustain', 'release'];
+
+  var between = function(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+  };
+
+  var addMouseHandlers = function(params) {
+    var mouseUpHandler = function(e) {
+      envelopeSvg.style.cursor = '';
+      document.removeEventListener('mousemove', timeDragHandler);
+      document.removeEventListener('mousemove', valueTimeDragHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    var timeDragHandler = function(e) {
+      var x = parseInt(params.timeRect.getAttributeNS(null, 'x'), 10);
+      var newX = between(e.offsetX - (params.previousX + minSpacing), 0, maxSpacing);
+      var paramTime = newX/maxSpacing;
+      audioEnvelope[params.name + 'Time'] = paramTime;
+      data.envelopeChanged = true;
+    };
+
+    var valueTimeDragHandler = function(e) {
+      var y = parseInt(params.valueTimeRect.getAttributeNS(null, 'y'), 10);
+      var newY = between(e.clientY - marginY, 0, drawHeight);
+      var paramValue = 1 - (newY/drawHeight);
+      audioEnvelope[params.name + 'Value'] = paramValue;
+      if (params.name === 'sustain' || params.name === 'decay') {
+        audioEnvelope.decayValue = paramValue;
+        audioEnvelope.sustainValue = paramValue;
+      }
+      timeDragHandler(e);
+      data.envelopeChanged = true;
+    };
+
+    var startTimeDrag = function(e) {
+      envelopeSvg.style.cursor = 'ew-resize';
+      document.addEventListener('mousemove', timeDragHandler);
+      document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    var startValueTimeDrag = function(e) {
+      if (params.name === 'sustain') {
+        envelopeSvg.style.cursor = 'ns-resize';
+      } else {
+        envelopeSvg.style.cursor = 'move';
+      }
+      document.addEventListener('mousemove', valueTimeDragHandler);
+      document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    var overTime = function(e) {
+      if (envelopeSvg.style.cursor === '') {
+        params.timeRect.style.cursor = 'ew-resize';
+      }
+    };
+
+    var leaveTime = function(e) {
+      if (envelopeSvg.style.cursor === '') {
+        params.timeRect.style.cursor = '';
+      }
+    };
+
+    var overValueTime = function(e) {
+      if (envelopeSvg.style.cursor === '') {
+        params.valueTimeRect.style.cursor = (params.name === 'sustain') ? 'ns-resize' : 'move';
+      }
+    };
+
+    var leaveValueTime = function(e) {
+      if (envelopeSvg.style.cursor === '') {
+        params.valueTimeRect.style.cursor = '';
+      }
+    };
+
+    if (params.name !== 'sustain') {
+      params.timeRect.addEventListener('mousedown', startTimeDrag);
+      params.timeRect.addEventListener('mouseover', overTime);
+      params.timeRect.addEventListener('mouseleave', leaveTime);
+    }
+    params.valueTimeRect.addEventListener('mousedown', startValueTimeDrag);
+    params.valueTimeRect.addEventListener('mouseover', overValueTime);
+    params.valueTimeRect.addEventListener('mouseleave', leaveValueTime);
+  };
+
+  var params = propNames.reduce(function(params, name) {
+    var elementParams = {
+      timeLine: document.getElementById('envelope-' + name + '-time'),
+      valueLine: document.getElementById('envelope-' + name + '-value'),
+      label: document.getElementById('envelope-' + name + '-label'),
+      timeRect: document.getElementById('envelope-' + name + '-time-rect'),
+      valueTimeRect: document.getElementById('envelope-' + name + '-value-time-rect'),
+      value: name + 'Value',
+      time: name + 'Time',
+      name: name,
+      previousX: 0
+    };
+    addMouseHandlers(elementParams);
+    params.push(elementParams);
+    return params;
+  }, []);
+
+  var previousY = bottomY - (audioEnvelope.delayValue * drawHeight);
+  var previousX = marginX;
+
+  var updateLine = function(line, x1, y1, x2, y2) {
+    line.setAttributeNS(null, 'x1', x1);
+    line.setAttributeNS(null, 'y1', y1);
+    line.setAttributeNS(null, 'x2', x2);
+    line.setAttributeNS(null, 'y2', y2);
+  };
+
+  var updateLabel = function(label, x, y) {
+    label.setAttributeNS(null, 'x', x);
+    label.setAttributeNS(null, 'y', y);
+  };
+
+  var updateRect = function(rect, x, y, width, height) {
+    rect.setAttributeNS(null, 'x', x);
+    rect.setAttributeNS(null, 'y', y);
+    rect.setAttributeNS(null, 'width', width);
+    rect.setAttributeNS(null, 'height', height);
+  };
+
+  params.forEach(function(param, index) {
+    var currentY = bottomY - (audioEnvelope[param.value] * drawHeight);
+    var currentX = previousX + minSpacing + (audioEnvelope[param.time] * maxSpacing);
+    param.previousX = previousX;
+    currentX = (param.name === 'sustain') ? releaseMin : currentX;
+    updateLine(param.valueLine, previousX, previousY, currentX, currentY);
+    updateLine(param.timeLine, currentX, 0, currentX, height);
+    updateLabel(param.label, previousX + 3, bottomY + 12);
+    updateRect(param.timeRect, currentX - 5, 0, 10, height);
+    updateRect(param.valueTimeRect, currentX - 5, currentY - 5, 10, 10);
+    previousX = currentX;
+    previousY = currentY;
+  });
+
+};
+
+module.exports = updateEnvelope;
